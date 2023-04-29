df1217a9544d53d940beb53a0471748b327387b0
BHUVISBRS <bhuvi@sbrs@gmail.com>
2023-04-29 21:23:17 +0530

    final

--- task12.js
+++ task12.js
@@ -1,7 +1,16 @@
 var form = document.getElementById('Forms');
 var itemList = document.getElementById('items');
+
+
+const studentsContainer = document.getElementById('.students');
+const nameInput =form["name"];
+const emailInput =form["email"];
+const phonenumberInput =form["phonenumber"];
+
 // Add item
 form.addEventListener('submit',addItem);
+
+const students = JSON.parse(localStorage.getItem("students")) || [];
 
 function addItem(e){
     e.preventDefault();
@@ -30,17 +39,37 @@ itemList.addEventListener('click',removeItem);
 
 var deleteBtn = document.createElement('button');
 deleteBtn.className=('btn btn-danger btn-sm float-right delete');
-deleteBtn.appendChild(document.createTextNode('delete'));
+deleteBtn.appendChild(document.createTextNode('Delete'));
 li.appendChild(deleteBtn);
 itemList.appendChild(li);
+
+
+    form.onsubmit = e => {
+    e.preventDefault();
+  
+    const newStudent = students(
+      nameInput.value,
+      emailInput.value,
+      phonenumberInput.value
+    );
+  
+    createStudentElement(newStudent);
+  
+    nameInput.value = "";
+    emailInput.value = "";
+    phonenumberInput.value = "";
+  };
 }
 function removeItem(e){
     if(e.target.classList.contains('delete')){
         if(confirm('Are You Sure')){
             var li = e.target.parentElement;
-            itemList.removeChild(li);
+            itemList.removeChild(li);}
+     
+
+            
+    
         }
-    }
 }
 
 
